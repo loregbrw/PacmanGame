a47d20096cd465e487619907e9e9030f08b10a5a
babislz <gabriela.laureano2007@gmail.com>
2023-11-14 23:56:40 -0300

    terminar a lógica

--- print_animacao.c
+++ print_animacao.c
@@ -9,101 +9,9 @@
 ma_result result;
 ma_engine engine;
 
-typedef struct
-{
-    int x, y, value1, value2;
-} Character;
-
-char user_input;
-Character pacman_player, ghost1, ghost2, ghost3, ghost4;
-int background[20][20], game_over_background[20][20], score;
-bool game_over;
-
 int animacao_um[20][20];
 int animacao_dois[20][20];
 int animacao_tres[20][20];
-
-void printWall(int x, int y);
-
-void defineBackground() // redefine o fundo para o padrão inicial
-{
-    FILE *matrix = fopen("./bg_menu.txt", "r");
-
-    int line = 0, col = 0;
-    char c;
-
-    while ((c = fgetc(matrix)) != EOF)
-    {
-        if (c == '\r')
-        {
-            continue;
-        }
-        if (c == '\n')
-        {
-            col = 0;
-            line++;
-            continue;
-        }
-        background[line][col++] = c - '0';
-    }
-    fclose(matrix);
-
-    //changePositions(&pacman_player, 18, 1);
-    
-}
-
-void printMatrix()
-{
-    HIDE_CURSOR();
-    MOVE_HOME();
-
-    for (int i = 0; i < 20; i++)
-    {
-        for (int  j = 0; j < 20; j++)
-        {
-            if (background[i][j] == 5)
-            {
-                FOREGROUND_COLOR(222, 7, 50);
-                printf("%c ", 208);
-                RESET_FOREGROUND();
-            }
-            else if (background[i][j] == 2)
-            {
-                FOREGROUND_COLOR(196, 173, 153);
-                printf("%c ", 250);
-                RESET_FOREGROUND();
-            }
-            else if (background[i][j] == 0)
-            {
-                printf("\e[1C");
-            }
-            else if (background[i][j] == 3)
-            {
-                FOREGROUND_COLOR(250, 177, 7);
-                printf("C ");
-                RESET_FOREGROUND();
-            }
-            else if (background[i][j] == 4)
-            {
-                FOREGROUND_COLOR(50, 201, 68);
-                printf("W ");
-                RESET_FOREGROUND();
-            }
-            else
-            {
-                FOREGROUND_COLOR(5, 125, 245);
-                printWall(j, i);
-                RESET_FOREGROUND();
-            }
-        }
-        printf("\n");
-    }
-    printf("\n y: %i - x: %i - score: %i", pacman_player.y, pacman_player.x, score);
-    ERASE_LEND();
-    MOVE_HOME();
-
-    fflush(stdout);        
-}
 
 void AnimacaoUm() // redefine o fundo para a tela de game over
     {
@@ -226,15 +134,72 @@ void AnimacaoUm() // redefine o fundo para a tela de game over
                 {
                     printf("t");
                 }
+
             }
             printf("\n");
     }
 
-    printMatrix();
-
     ERASE_LEND();
 
     fflush(stdout);
+    }
+
+    void printWall(int x, int y, int animacao[20][20])
+    {
+        int up = 0, down = 0, left = 0, right = 0;
+
+        if (x != 19)
+        {
+            if (animacao[y][x+1] == 1) {right = 1;}
+        }
+        if (x != 0)
+        {
+            if (animacao[y][x-1] == 1) {left = 1;}
+        }
+        if (y != 19)
+        {
+            if (animacao[y+1][x] == 1) {down = 1;}
+        }
+        if (y != 0)
+        {
+            if (animacao[y-1][x] == 1) {up = 1;}
+        }
+
+        else if (up && right && down)
+        {
+            printf("%c%c", 204, 205);
+        }
+        else if (left && up)
+        {
+            printf("%c ", 188);
+        }
+        else if (left && down)
+        {
+            printf("%c ", 187);
+        }
+        else if (up && right)
+        {
+            printf("%c%c", 200, 205);
+        }
+        else if (right && down)
+        {
+            printf("%c%c", 201, 205);
+        }
+        else if (up || down)
+        {
+            printf("%c ", 186);
+        }
+        else if (left || right)
+        {
+            if (left && !right)
+            {
+                printf("%c ", 205);
+            }
+            else
+            {
+                printf("%c%c", 205, 205);
+            }
+    }
     }
 
 int main(){
@@ -244,14 +209,11 @@ int main(){
     AnimacaoDois();
     AnimacaoTres();
 
-
     result = ma_engine_init(NULL, &engine);  
     ma_engine_play_sound(&engine, "musica.mp3", NULL);
     Sleep(5000);
     for (int i = 0; i < 10; i++)
-    {
-        defineBackground();
-        
+    {   
         Coloridinho(animacao_um);
         Sleep(100);
         Coloridinho(animacao_dois);
@@ -262,5 +224,3 @@ int main(){
         Sleep(100);
     }
 }
-
-//ARRUMAR A CAGADA QUE O ANDREY FEZ
